0b1592f562f00f8b2896f699222ead86317082ac
Joanna <j_szczech@outlook.com>
2017-01-08 19:33:30 +0100

    Display logged user

--- js/initializeFirebase.js
+++ js/initializeFirebase.js
@@ -2,6 +2,7 @@ var config = {
     apiKey: "AIzaSyD_fXolbFIyRtphkqinzTUnauN0ow_pGOA",
     authDomain: "blog-dbcf4.firebaseapp.com",
     databaseURL: "https://blog-dbcf4.firebaseio.com",
-    storageBucket: ""
+    storageBucket: "blog-dbcf4.appspot.com",
+    messagingSenderId: "1099208186388"
 };
 firebase.initializeApp(config);
