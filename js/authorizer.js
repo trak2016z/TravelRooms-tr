9df67b6c7010aabbe788fdd8abbcf09a75bbfae3
Joanna <j_szczech@outlook.com>
2017-01-08 19:35:12 +0100

    Update Firebase

--- js/authorizer.js
+++ js/authorizer.js
@@ -1,10 +1,10 @@
 firebase.auth().onAuthStateChanged(function(user) {
-    // if (user) {
-    //     document.getElementById('userAccount').innerHTML = getUserName(user);
-    // }
-    // else if (window.location.pathname !== "/Blog-master/Blog-master/homePage.html") {
-    //     window.location.assign("homePage.html");
-    // }
+    if (user) {
+        document.getElementById('userAccount').innerHTML = getUserName(user);
+    }
+    else if (window.location.pathname !== "/Blog-master/Blog-master/homePage.html") {
+        window.location.assign("homePage.html");
+    }
 });
 
 function getUserName(user){
